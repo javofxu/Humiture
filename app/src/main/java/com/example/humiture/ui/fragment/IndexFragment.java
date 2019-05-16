7c6265add900000fdb72c15a763e85ab429b2fb4
javofxu <1805298170@qq.com>
2019-05-16 21:28:58 +0800

    横向滑动切换环境数据
    环境数据切换滑动+定位显示

--- app/src/main/java/com/example/humiture/ui/fragment/IndexFragment.java
+++ app/src/main/java/com/example/humiture/ui/fragment/IndexFragment.java
@@ -1,18 +1,78 @@
 package com.example.humiture.ui.fragment;
 
 import android.support.v4.app.Fragment;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.base.BaseFragment;
 import com.example.humiture.R;
+import com.example.humiture.R2;
+import com.example.humiture.ui.view.adapter.LoopShowAdapter;
+import com.example.humiture.utils.ItemDecorationUtils;
+import com.yarolegovich.discretescrollview.DiscreteScrollView;
+
+import java.util.HashMap;
+
+import butterknife.BindView;
 
 /**
+ * Created by 许格.
+ * Date on 2019/5/14.
  * A simple {@link Fragment} subclass.
  * 首页
  */
 public class IndexFragment extends BaseFragment {
 
+    @BindView(R2.id.picker)
+    DiscreteScrollView picker;
+    @BindView(R2.id.index_point)
+    LinearLayout point;
+
+    LoopShowAdapter adapter;
+    private ImageView[] mImageViews;
+    private int pagerNumber;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_index;
     }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        pagerNumber = 4;
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put(ItemDecorationUtils.LEFT_DECORATION,20);//右间距
+        map.put(ItemDecorationUtils.RIGHT_DECORATION,20);//右间距
+        adapter = new LoopShowAdapter(mContext,pagerNumber);
+        picker.addItemDecoration(new ItemDecorationUtils(map));
+        picker.setAdapter(adapter);
+        picker.setCurrentItemChangeListener((viewHolder, adapterPosition) -> {
+            drawPoint(adapterPosition);
+        });
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+
+    void drawPoint(int position){
+        point.removeAllViews();
+        mImageViews = new ImageView[pagerNumber];
+        for (int i = 0; i < pagerNumber; i++) {
+            ImageView imageView = new ImageView(mContext);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(10, 0, 10, 0);
+            imageView.setLayoutParams(params);
+            if(i==position){
+                imageView.setImageResource(R.mipmap.index_yuan_sel);
+            }
+            else {
+                imageView.setImageResource(R.mipmap.index_yuan);
+            }
+            mImageViews[i]=imageView;
+            point.addView(imageView);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/humiture/ui/view/adapter/LoopShowAdapter.java
@@ -0,0 +1,103 @@
+package com.example.humiture.ui.view.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.humiture.R;
+import com.example.humiture.R2;
+import com.example.humiture.ui.view.WaveLoadingView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by 许格.
+ * Date on 2019/5/16.
+ * dec:
+ */
+public class LoopShowAdapter extends RecyclerView.Adapter<LoopShowAdapter.ItemHolder>{
+
+    private Context context;
+    private int pagerNumber;
+
+    public LoopShowAdapter(Context context,int size) {
+        this.context = context;
+        this.pagerNumber = size;
+    }
+
+    @Override
+    public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items_index_wave, parent,false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemHolder holder, int position) {
+        switch (position){
+            case 0:
+                holder.left_wave.setWaveColor(0xffdc4d9d);
+                holder.left_wave.setTopTitle("11");
+                holder.left_icon.setImageResource(R.mipmap.icon_temperature);
+                holder.left_msg.setText(R.string.index_temp);
+                holder.right_wave.setWaveColor(0xff04b9e5);
+                holder.right_icon.setImageResource(R.mipmap.icon_humidity);
+                holder.right_msg.setText(R.string.index_hum);
+                break;
+            case 1:
+                holder.left_wave.setWaveColor(0xff11df99);
+                holder.left_icon.setImageResource(R.mipmap.icon_pm25);
+                holder.left_msg.setText(R.string.index_pm);
+                holder.right_wave.setWaveColor(0xffb3d25a);
+                holder.right_icon.setImageResource(R.mipmap.icon_bacterial);
+                holder.right_msg.setText(R.string.index_colony);
+                break;
+            case 2:
+                holder.left_wave.setWaveColor(0xff13b745);
+                holder.left_icon.setImageResource(R.mipmap.icon_formol);
+                holder.left_msg.setText(R.string.index_formaldehyde);
+                holder.right_wave.setWaveColor(0xfff2c107);
+                holder.right_icon.setImageResource(R.mipmap.icon_tvoc);
+                holder.right_msg.setText(R.string.index_tv);
+                break;
+            case 3:
+                holder.left_wave.setWaveColor(0xff8f31e1);
+                holder.left_icon.setImageResource(R.mipmap.icon_gas);
+                holder.left_msg.setText(R.string.index_harmful);
+                holder.right_wave.setWaveColor(0xff75cfea);
+                holder.right_icon.setImageResource(R.mipmap.icon_eco2);
+                holder.right_msg.setText(R.string.index_eco);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return pagerNumber;
+    }
+
+    class ItemHolder extends RecyclerView.ViewHolder {
+        @BindView(R2.id.item_left_wave)
+        WaveLoadingView left_wave;
+        @BindView(R2.id.item_right_wave)
+        WaveLoadingView right_wave;
+        @BindView(R2.id.item_left_icon)
+        ImageView left_icon;
+        @BindView(R2.id.item_right_icon)
+        ImageView right_icon;
+        @BindView(R2.id.item_left_msg)
+        TextView left_msg;
+        @BindView(R2.id.item_right_msg)
+        TextView right_msg;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
