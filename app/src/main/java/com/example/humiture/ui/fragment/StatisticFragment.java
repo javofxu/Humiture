f59e65c494bc3656641dff80bd69816fb197011f
Micheal <wdou_zhangxuetao@163.com>
2019-05-17 17:02:46 +0800

    添加报警统计
    添加报警统计信息

--- app/src/main/java/com/example/humiture/ui/fragment/StatisticFragment.java
+++ app/src/main/java/com/example/humiture/ui/fragment/StatisticFragment.java
@@ -1,11 +1,18 @@
 package com.example.humiture.ui.fragment;
 
 import android.support.v4.app.Fragment;
+import android.widget.ScrollView;
 
 import com.example.base.BaseFragment;
 import com.example.humiture.R;
+import com.example.humiture.R2;
+import com.example.humiture.mvp.contract.StatisticContract;
+import com.example.humiture.mvp.presenter.IndexPresent;
+import com.example.humiture.mvp.presenter.StatisticPresent;
 import com.example.humiture.ui.view.RadarData;
 import com.example.humiture.ui.view.RadarView;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +22,12 @@ import butterknife.BindView;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StatisticFragment extends BaseFragment {
+public class StatisticFragment extends BaseFragment<StatisticPresent> implements StatisticContract.mView {
 
-    @BindView(R.id.radarView)
+    @BindView(R2.id.radarView)
     RadarView radarView;
+    @BindView(R2.id.pie_chart)
+    PieChart pieChart;
 
     @Override
     protected int getLayoutId() {
@@ -26,9 +35,28 @@ public class StatisticFragment extends BaseFragment {
     }
 
     @Override
+    protected void initPresent() {
+        super.initPresent();
+        mPresent = new StatisticPresent(mContext);
+    }
+
+    @Override
     protected void initView() {
         super.initView();
 
+        //雷达图数据
+        RadarData();
+
+        //饼图
+        mPresent.showPieChart(pieChart,getPieChartData());
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+
+    private void RadarData(){
         List<RadarData> dataList = new ArrayList<>();
         for (int i = 1; i < 6; i++) {
             //传入数据
@@ -38,8 +66,15 @@ public class StatisticFragment extends BaseFragment {
         radarView.setDataList(dataList);
     }
 
-    @Override
-    protected void initData() {
-        super.initData();
+    private List<PieEntry> getPieChartData(){
+        List<PieEntry> dataList = new ArrayList<>();
+        dataList.add(new PieEntry(20,"菌落"));
+        dataList.add(new PieEntry(30,"有害气体"));
+        dataList.add(new PieEntry(40,"设备"));
+        dataList.add(new PieEntry(50,"EOC2"));
+        dataList.add(new PieEntry(60,"温度"));
+        dataList.add(new PieEntry(70,"甲醛"));
+        return dataList;
     }
+
 }
